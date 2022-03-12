460d5ea145aec0b54b5d9f39ec241ad0e3c379c7
Ahmed Hallaba <am00767@gmail.com>
2022-03-12 18:05:27 +0200

    migrations

--- database/migrations/2022_03_12_154040_create_floods_table.php
+++ database/migrations/2022_03_12_154040_create_floods_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('floods', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamps();
+            $table->double('lat')->nullable();
+            $table->double('lng')->nullable();
+            $table->boolean('is_active');
+            $table->unsignedBigInteger('added_by');
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('floods');
     }
 };
--- database/migrations/2022_03_12_154602_create_requests_table.php
+++ database/migrations/2022_03_12_154602_create_requests_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('requests', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->unsignedBigInteger('civilian_id');
+            $table->boolean('is_approved');
+            $table->unsignedBigInteger('approved_by');
+            $table->double('lat')->nullable();
+            $table->double('lng')->nullable();
+            $table->string('img')->default('images/default.png');
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('requests');
     }
 };
