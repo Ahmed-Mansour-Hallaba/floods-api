b36f4157c606707cfb1d258d13e5c031404b9767
Ahmed Hallaba <am00767@gmail.com>
2022-03-17 00:59:57 +0200

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\CiviliansController;
+use App\Http\Controllers\FloodsController;
 use App\Http\Controllers\RegisterController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\RequestsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,7 +28,17 @@ Route::controller(CiviliansController::class)->group(function () {
     Route::post('civilian/register', 'register');
 });
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Authenticated routes
+Route::middleware('auth:sanctum')->group(function () {
+    //civilians routes
+    Route::post('civilian/update',[CiviliansController::class,'update']);
+    //Flood routes
+    Route::get('flood/all',[FloodsController::class,'getAllActive']);
+    Route::post('flood/create',[FloodsController::class,'create']);
+    Route::post('flood/update',[FloodsController::class,'update']);
+    //Requests routes
+    Route::get('request/pending',[RequestsController::class,'gettAllPending']);
+    Route::get('request/all',[RequestsController::class,'gettAll']);
+    Route::post('request/create',[RequestsController::class,'create']);
+    Route::post('request/update',[RequestsController::class,'update']);
 });
